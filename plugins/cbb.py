ceaf16c226e0e2d21e81ee3b5233e4e69eb41853
vamsi899 <krishnavamsibakka09@gmail.com>
2025-05-02 16:53:22 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>🤖 My Name :</b> <a href='https://t.me/Cartoonworldtelugu_india09_bot'>File Sharing Bot</a> \n<b>📝 Language :</b> <a href='https://python.org'>Python 3</a> \n<b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram {__version__}</a> \n<b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a> \n<b>📢 Channel :</b> <a href='https://t.me/Madflix_Bots'>Madflix Botz</a> \n<b>🧑‍💻 Developer :</b> <a href='tg://user?id={OWNER_ID}'>Jishu Developer</a>",
+            text = f"<b>🤖 My Name :</b> <a href='https://t.me/Cartoonworldtelugu_india09_bot'>File Sharing Bot</a> \n<b>📝 Language :</b> <a href='https://python.org'>Python 3</a> \n<b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram {__version__}</a> \n<b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a> \n<b>📢 Channel :</b> <a href='https://t.me/Cartoonworldtelugu_india'>Cartoonworldtelugu_india</a> \n<b>🧑‍💻 Developer :</b> <a href='tg://user?id={6828129421}'>𝐕𝐚𝐦𝐬𝐢 🐦‍🔥</a>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
@@ -32,6 +32,5 @@ async def cb_handler(client: Bot, query: CallbackQuery):
 
 
 # Vamsi
-# Don't Remove Credit
 # Telegram Channel @Cartoonworldtelugu_india
 # Developer @Vamsi_2008_Vamsi
